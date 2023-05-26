0dc834012b14ca64d62a8d32085158b7fc9786be
KSullivan <Sullylifts@gmail.com>
2023-05-26 10:48:57 -0500

    Create Connect Example.py

---
+++ MSSQL/Connect Example.py
@@ -0,0 +1,21 @@
+import hetpy as hp
+import pyodbc
+import pandas as pd
+
+# Set functions
+def ms_connect(user,passw,server_name,db,driver):
+    cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server_name + ';DATABASE=' + db + ';UID=' + user+ ';PWD=' + passw)
+    return cnxn
+
+print('Enter Credentials...')
+user = hp.inputter('Enter Username: ','str')
+passw = hp.input_pass()
+
+print('Connecting...')
+cnxn = ms_connect(user,passw,'YOUR_SERVER_HERE','YOUR_DB_HERE','SQL Server')
+
+cnxn.execute('SELECT TOP 5 * INTO #TMP_Table FROM YOUR_DB_HERE.dbo.Table')
+
+df = pd.read_sql('SELECT * FROM #TMP_Table',cnxn)
+
+
